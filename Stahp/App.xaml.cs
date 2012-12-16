e6e5e628e832a0ca6cc0d4fd463af6e0c78f55ac
Tobias Walter <tobias.walter@gmx.net>
2012-12-16 11:53:47 +0100

    Initial commit.

---
+++ Stahp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Stahp
+{
+    public partial class App
+    {
+        private TaskbarIcon tb;
+
+        protected override void OnStartup(System.Windows.StartupEventArgs e)
+        {
+            tb = (TaskbarIcon)FindResource("MyNotifyIcon");
+
+            var viewModel = new StahpViewModel();
+            viewModel.Notify += (s, evt) => tb.ShowBalloonTip("Stahp!!!", "Time's up.", BalloonIcon.Error);
+
+            tb.DataContext = viewModel;
+        }
+
+        protected override void OnExit(System.Windows.ExitEventArgs e)
+        {
+            tb.Dispose();
+        }
+    }
+}
---
+++ Stahp/StahpViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+using Stahp.Annotations;
+
+namespace Stahp
+{
+    public class StahpViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<EventArgs> Notify;
+
+        private readonly DispatcherTimer timer;
+        private DateTime? startAction;
+        private TimeSpan restTime;
+        private string totalRestTime;
+
+        public StahpViewModel()
+        {
+            TotalRestTime = TimeSpan.FromHours(2).ToString();
+            restTime = TimeSpan.Parse(TotalRestTime);
+
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(5) };
+
+            InterceptMouse.Start();
+            InterceptMouse.MouseAction += (s, e) =>
+            {
+                if (restTime == TimeSpan.Zero)
+                {
+                    OnNotify();
+                    return;
+                }
+
+                if (startAction.HasValue && DateTime.Now.Subtract(startAction.Value) > restTime)
+                {
+                    restTime = TimeSpan.Zero;
+                    startAction = null;
+                    timer.Stop();
+                    timer.IsEnabled = false;
+
+                    OnPropertyChanged("RestTime");
+
+                    return;
+                }
+                
+                if (startAction == null)
+                    startAction = DateTime.Now;
+
+                timer.Stop();
+                timer.Start();
+
+                OnPropertyChanged("RestTime");
+            };
+
+            timer.Tick += (s, e) =>
+            {
+                if (startAction.HasValue)
+                    restTime = restTime.Subtract(DateTime.Now.Subtract(startAction.Value));
+
+                timer.Stop();
+                startAction = null;
+            };
+        }
+
+        public string TotalRestTime
+        {
+            get { return totalRestTime; }
+            set
+            {
+                totalRestTime = value;
+
+                restTime = DateTime.Parse(TotalRestTime).TimeOfDay;
+            }
+        }
+
+        public TimeSpan RestTime
+        {
+            get
+            {
+                if (startAction.HasValue)
+                    return restTime.Subtract(DateTime.Now.Subtract(startAction.Value));
+
+                return restTime;
+            }
+        }
+
+        protected virtual void OnNotify()
+        {
+            EventHandler<EventArgs> handler = Notify;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            InterceptMouse.Stop();
+        }
+    }
+}
